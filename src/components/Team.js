59e3f53818c4640a1a9026c7c7f12ec5d88f39ee
Kaushik-Shahare <kaushikshahare0@gmail.com>
2024-03-07 15:32:56 +0530

    created team section

--- src/components/Team.js
+++ src/components/Team.js
@@ -3,7 +3,132 @@ import React from "react";
 function Team() {
   return (
     <div className="Team Card cardLeft">
-    <h1>Team</h1>
+      <h1>Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Sem et tortor
+        consequat id porta nibh venenatis. Nibh ipsum consequat nisl vel pretium
+        lectus quam id leo. Convallis convallis tellus id interdum velit laoreet
+        id donec ultrices. Pellentesque elit eget gravida cum. In fermentum et
+        sollicitudin ac orci phasellus. Ipsum a arcu cursus vitae.
+      </p>
+      <div
+        style={{
+          background: "	#cfecf7",
+          marginTop: "20px",
+          padding: "20px",
+          borderRadius: "10px",
+        }}
+        className="d-lg-flex justify-content-between align-items-center"
+      >
+        <div>
+          <img
+            style={{
+              height: "15vh",
+              display: "block",
+              textAlign: "center",
+              marginLeft: "auto",
+              marginRight: "auto",
+              borderRadius: "15px",
+            }}
+            src="/JohnSmith.jpeg"
+          />
+          <h1 style={{ textAlign: "center" }}>John Smith</h1>
+          <p style={{ textAlign: "center" }}>Designation Here</p>
+        </div>
+        <p style={{ marginLeft: "20px" }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem et
+          tortor consequat id porta nibh venenatis. Nibh ipsum consequat nisl
+          vel pretium lectus quam id leo. Convallis convallis tellus id interdum
+          velit laoreet id donec ultrices. Pellentesque elit eget gravida cum.
+          In fermentum et sollicitudin ac orci phasellus. Ipsum a arcu cursus
+          vitae. Aliquam id diam maecenas ultricies mi eget mauris pharetra.
+          Cursus risus at ultrices mi tempus imperdiet. Cursus sit amet dictum
+          sit amet justo. Lectus sit amet est placerat in. Sit amet massa vitae
+          tortor condimentum lacinia quis vel eros. Non quam lacus suspendisse
+          faucibus interdum posuere. Venenatis urna cursus eget nunc scelerisque
+          viverra mauris in aliquam. At risus viverra adipiscing at in.
+        </p>
+      </div>
+      <div
+        style={{
+          background: "	#cfecf7",
+          marginTop: "20px",
+          padding: "20px",
+          borderRadius: "10px",
+        }}
+        className="d-lg-flex justify-content-between align-items-center"
+      >
+        <div>
+          <img
+            style={{
+              height: "15vh",
+              display: "block",
+              textAlign: "center",
+              marginLeft: "auto",
+              marginRight: "auto",
+              borderRadius: "15px",
+            }}
+            src="/JohnSmith.jpeg"
+          />
+          <h1 style={{ textAlign: "center" }}>John Smith</h1>
+          <p style={{ textAlign: "center" }}>Designation Here</p>
+        </div>
+        <p style={{ marginLeft: "20px" }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem et
+          tortor consequat id porta nibh venenatis. Nibh ipsum consequat nisl
+          vel pretium lectus quam id leo. Convallis convallis tellus id interdum
+          velit laoreet id donec ultrices. Pellentesque elit eget gravida cum.
+          In fermentum et sollicitudin ac orci phasellus. Ipsum a arcu cursus
+          vitae. Aliquam id diam maecenas ultricies mi eget mauris pharetra.
+          Cursus risus at ultrices mi tempus imperdiet. Cursus sit amet dictum
+          sit amet justo. Lectus sit amet est placerat in. Sit amet massa vitae
+          tortor condimentum lacinia quis vel eros. Non quam lacus suspendisse
+          faucibus interdum posuere. Venenatis urna cursus eget nunc scelerisque
+          viverra mauris in aliquam. At risus viverra adipiscing at in.
+        </p>
+      </div>
+      <div
+        style={{
+          background: "	#cfecf7",
+          marginTop: "20px",
+          padding: "20px",
+          borderRadius: "10px",
+        }}
+        className="d-lg-flex justify-content-between align-items-center"
+      >
+        <div>
+          <img
+            style={{
+              height: "15vh",
+              display: "block",
+              textAlign: "center",
+              marginLeft: "auto",
+              marginRight: "auto",
+              borderRadius: "15px",
+            }}
+            src="/JohnSmith.jpeg"
+          />
+          <h1 style={{ textAlign: "center" }}>John Smith</h1>
+          <p style={{ textAlign: "center" }}>Designation Here</p>
+        </div>
+        <p style={{ marginLeft: "20px" }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem et
+          tortor consequat id porta nibh venenatis. Nibh ipsum consequat nisl
+          vel pretium lectus quam id leo. Convallis convallis tellus id interdum
+          velit laoreet id donec ultrices. Pellentesque elit eget gravida cum.
+          In fermentum et sollicitudin ac orci phasellus. Ipsum a arcu cursus
+          vitae. Aliquam id diam maecenas ultricies mi eget mauris pharetra.
+          Cursus risus at ultrices mi tempus imperdiet. Cursus sit amet dictum
+          sit amet justo. Lectus sit amet est placerat in. Sit amet massa vitae
+          tortor condimentum lacinia quis vel eros. Non quam lacus suspendisse
+          faucibus interdum posuere. Venenatis urna cursus eget nunc scelerisque
+          viverra mauris in aliquam. At risus viverra adipiscing at in.
+        </p>
+      </div>
     </div>
   );
 }
